d8e19c9488caeecd70cf75d3105074c21bae9f2d
Robin Kolk <robinkolk@msn.com>
2021-08-30 20:21:48 +0200

    first try at fixing fan issues
    migrating to new Homeassistant standards.

--- custom_components/miele/fan.py
+++ custom_components/miele/fan.py
@@ -3,6 +3,10 @@ from datetime import timedelta
 
 from homeassistant.components.fan import SUPPORT_SET_SPEED, FanEntity
 from homeassistant.helpers.entity import Entity
+from homeassistant.util.percentage import int_states_in_range, ranged_value_to_percentage, percentage_to_ranged_value
+import math
+from typing import Optional
+
 
 from custom_components.miele import DATA_CLIENT, DATA_DEVICES
 from custom_components.miele import DOMAIN as MIELE_DOMAIN
@@ -16,7 +20,7 @@ ALL_DEVICES = []
 SUPPORTED_TYPES = [18]
 
 
-OPERATION_SPEEDS = [0, 1, 2, 3, 4]
+SPEED_RANGE = (1, 4)
 
 
 # pylint: disable=W0612
@@ -51,6 +55,7 @@ class MieleFan(FanEntity):
         self._hass = hass
         self._device = device
         self._ha_key = "fan"
+        self._current_speed = 0
 
     @property
     def device_id(self):
@@ -84,26 +89,36 @@ class MieleFan(FanEntity):
         return SUPPORT_SET_SPEED
 
     @property
-    def speed_list(self) -> list:
-        """Get the list of available speeds."""
-        return OPERATION_SPEEDS
-
-    @property
     def speed(self):
         """Return the current speed"""
         return self._device["state"]["ventilationStep"]["value_raw"]
 
-    def turn_on(self, speed=None, **kwargs) -> None:
+    @property
+    def percentage(self) -> Optional[int]:
+        """Return the current speed percentage."""
+        return ranged_value_to_percentage(SPEED_RANGE, self._current_speed)
+
+    @property
+    def speed_count(self) -> int:
+        """Return the number of speeds the fan supports."""
+        return int_states_in_range(SPEED_RANGE)
+
+    def turn_on(self, percentage: Optional[int] = None, **kwargs) -> None:
         """Turn on the fan."""
+        value_in_range = math.ceil(percentage_to_ranged_value(SPEED_RANGE, percentage))
+        if percentage == "0":
+            self.turn_off()
         client = self._hass.data[MIELE_DOMAIN][DATA_CLIENT]
         client.action(device_id=self.device_id, body={"powerOn": True})
 
-    async def async_turn_on(self, speed=None, **kwargs):
+    async def async_turn_on(self, percentage: Optional[int] = None, **kwargs):
         """Turn on the fan."""
-        if speed == "0":
+        if percentage == "0":
             await self.async_turn_off()
-        elif speed is not None:
-            await self.async_set_speed(speed=speed)
+        elif percentage is not None:
+            await self.async_set_percentage(percentage=percentage)
+            client = self._hass.data[MIELE_DOMAIN][DATA_CLIENT]
+            await client.action(device_id=self.device_id, body={"powerOn": True})
         else:
             _LOGGER.debug("Turning on")
             client = self._hass.data[MIELE_DOMAIN][DATA_CLIENT]
@@ -118,10 +133,21 @@ class MieleFan(FanEntity):
         client = self._hass.data[MIELE_DOMAIN][DATA_CLIENT]
         await client.action(device_id=self.device_id, body={"powerOff": True})
 
-    async def async_set_speed(self, speed):
-        _LOGGER.debug("Setting speed to : {}".format(speed))
+    def set_percentage(self, percentage: int) -> None:
+        """Set the speed percentage of the fan."""  # TODO:
+        value_in_range = math.ceil(percentage_to_ranged_value(SPEED_RANGE, percentage))
+        self._current_speed = value_in_range
+        _LOGGER.debug("Setting speed to : {}".format(value_in_range))
         client = self._hass.data[MIELE_DOMAIN][DATA_CLIENT]
-        await client.action(device_id=self.device_id, body={"ventilationStep": speed})
+        client.action(device_id=self.device_id, body={"ventilationStep": value_in_range})
+
+    async def async_set_percentage(self, percentage: int) -> None:
+        """Set the speed percentage of the fan.""" #
+        value_in_range = math.ceil(percentage_to_ranged_value(SPEED_RANGE, percentage))
+        self._current_speed = value_in_range
+        _LOGGER.debug("Setting speed to : {}".format(value_in_range))
+        client = self._hass.data[MIELE_DOMAIN][DATA_CLIENT]
+        await client.action(device_id=self.device_id, body={"ventilationStep": value_in_range})
 
     async def async_update(self):
         if not self.device_id in self._hass.data[MIELE_DOMAIN][DATA_DEVICES]:
